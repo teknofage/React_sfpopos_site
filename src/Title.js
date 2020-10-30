0a6fd9fe29865c284ff48c4a7120d1d98084e391
teknofage <teknofage@gmail.com>
2020-10-30 02:47:03 -0700

    header component built

---
+++ src/Title.js
@@ -0,0 +1,20 @@
+// src/Title.js
+
+import './Title.css';
+import React from 'react'
+
+function Title() {
+    return (
+        <div className="Title">
+            <header>
+                <h1>SFPOPOS</h1>
+                    <div className="Title-Subtitle">
+                        <h2>San Francisco Privately Owned Public Open Spaces</h2>
+                    </div>
+            </header>
+        </div>
+
+    )
+    }
+
+export default Title
